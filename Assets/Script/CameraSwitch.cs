85963d4288e6278c08d767b13c0b540d7773924b
WelkinTotoro <WelkinTotoro@outlook.com>
2018-11-11 03:12:06 +0800

    加入Cinemachine，尝试使用其控制摄像机

--- Assets/Script/CameraSwitch.cs
+++ Assets/Script/CameraSwitch.cs
@@ -2,12 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CameraSwitch : MonoBehaviour {
-
-    public  GameObject Camera;
-    public  GameObject Maincamera;
-    public GameObject Player;
-    bool IsChanged=false;
+public class CameraSwitch : MonoBehaviour
+{
+    public GameObject thisCamera;
+    //public GameObject mainCamera;
+    public GameObject player;
+    bool IsChanged = false;
     bool Isinrange = false;
 
     private void Start()
@@ -27,24 +27,26 @@ public class CameraSwitch : MonoBehaviour {
 
     void Update()
     {
-        if (Input.GetKey(KeyCode.E)&&(!IsChanged)&&(Isinrange))  
+        if (Input.GetKey(KeyCode.E) && (!IsChanged) && (Isinrange))
         {
             Debug.Log("0");
-            Camera.GetComponent<Camera>().enabled = true;
-            Maincamera.GetComponent<Camera>().enabled = false;
-            Player.GetComponent<PlayerMove>().enabled = false;
+            //Camera.GetComponent<Camera>().enabled = true;
+            //Maincamera.GetComponent<Camera>().enabled = false;
+            thisCamera.SetActive(true);
+            player.GetComponent<PlayerMove>().enabled = false;
             IsChanged = true;
             Debug.Log("1");
         }
 
-        if (Input.GetKey(KeyCode.Q) && (IsChanged)&&(Isinrange))
+        if (Input.GetKey(KeyCode.Q) && (IsChanged) && (Isinrange))
         {
-            Maincamera.GetComponent<Camera>().enabled = true;
-            Camera.GetComponent<Camera>().enabled = false;
-            Player.GetComponent<PlayerMove>().enabled = true;
+            //Maincamera.GetComponent<Camera>().enabled = true;
+            //Camera.GetComponent<Camera>().enabled = false;
+            thisCamera.SetActive(false);
+            player.GetComponent<PlayerMove>().enabled = true;
             IsChanged = false;
             Debug.Log("2");
         }
-       // Debug.Log("in");
+        // Debug.Log("in");
     }
 }
