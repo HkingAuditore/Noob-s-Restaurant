58d6dbbb5c1cbc862d4aa97e8079707dcedf5e02
WelkinTotoro <WelkinTotoro@outlook.com>
2018-11-12 12:52:36 +0800

    加入所有的镜头切换 改变切换判断逻辑为Trigger触发 重新烘焙场景(没啥用)

--- Assets/Script/CameraSwitch.cs
+++ Assets/Script/CameraSwitch.cs
@@ -7,46 +7,64 @@ public class CameraSwitch : MonoBehaviour
     public GameObject thisCamera;
     //public GameObject mainCamera;
     public GameObject player;
-    bool IsChanged = false;
-    bool Isinrange = false;
+    //bool IsChanged = false;
+    //bool Isinrange = false;
 
     private void Start()
     {
-        IsChanged = false;
+        //IsChanged = false;
     }
 
-    void OnCollisionEnter()
+    void OnTriggerEnter()
     {
-        Isinrange = true;
+        //Isinrange = true;
     }
 
-    void OnCollisionExit()
+    private void OnTriggerStay(Collider other)
     {
-        Isinrange = false;
-    }
+        if (!other.CompareTag("Player"))
+            return;
 
-    void Update()
-    {
-        if (Input.GetKey(KeyCode.E) && (!IsChanged) && (Isinrange))
+        if (Input.GetKey(KeyCode.E))
         {
-            Debug.Log("0");
-            //Camera.GetComponent<Camera>().enabled = true;
-            //Maincamera.GetComponent<Camera>().enabled = false;
             thisCamera.SetActive(true);
             player.GetComponent<PlayerMove>().enabled = false;
-            IsChanged = true;
-            Debug.Log("1");
         }
 
-        if (Input.GetKey(KeyCode.Q) && (IsChanged) && (Isinrange))
+        if (Input.GetKey(KeyCode.Q))
         {
-            //Maincamera.GetComponent<Camera>().enabled = true;
-            //Camera.GetComponent<Camera>().enabled = false;
             thisCamera.SetActive(false);
             player.GetComponent<PlayerMove>().enabled = true;
-            IsChanged = false;
-            Debug.Log("2");
         }
-        // Debug.Log("in");
     }
+
+    void OnTriggerExit()
+    {
+        //Isinrange = false;
+    }
+
+    //void Update()
+    //{
+    //    if (Input.GetKey(KeyCode.E) && (!IsChanged) && (Isinrange))
+    //    {
+    //        Debug.Log("0");
+    //        //Camera.GetComponent<Camera>().enabled = true;
+    //        //Maincamera.GetComponent<Camera>().enabled = false;
+    //        thisCamera.SetActive(true);
+    //        player.GetComponent<PlayerMove>().enabled = false;
+    //        IsChanged = true;
+    //        Debug.Log("1");
+    //    }
+
+    //    if (Input.GetKey(KeyCode.Q) && (IsChanged) && (Isinrange))
+    //    {
+    //        //Maincamera.GetComponent<Camera>().enabled = true;
+    //        //Camera.GetComponent<Camera>().enabled = false;
+    //        thisCamera.SetActive(false);
+    //        player.GetComponent<PlayerMove>().enabled = true;
+    //        IsChanged = false;
+    //        Debug.Log("2");
+    //    }
+    //    // Debug.Log("in");
+    //}
 }
