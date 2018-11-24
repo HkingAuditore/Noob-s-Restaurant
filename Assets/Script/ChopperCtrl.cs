483f723f23634eadaa35ad5785dc4e93e40eaf40
HkingAuditore <178854663@qq.com>
2018-11-25 01:23:23 +0800

    修复
    1.合并了大部分的建模，厨房目前尚未改完。
    2.修改了菜刀控制的代码。

--- Assets/Script/ChopperCtrl.cs
+++ Assets/Script/ChopperCtrl.cs
@@ -7,6 +7,7 @@ public class ChopperCtrl : MonoBehaviour
     public bool isCtrlling = false;
 
     private Vector3 oriPos;
+    private Rigidbody KnifeRB;
     public float offsetY = 0.3f;
     public float offsetZ = 0.8f;
 
@@ -17,13 +18,23 @@ public class ChopperCtrl : MonoBehaviour
     {
         oriPos = transform.position;
         lastMousePos = new Vector3(Screen.width / 2, Screen.height / 2, 0);
+        KnifeRB = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
         if (!isCtrlling)
+        {
+            KnifeRB.useGravity = true;          //当玩家不使用时受重力控制
+            KnifeRB.isKinematic = false;
             return;
+        }
+
+        this.transform.rotation = Quaternion.Euler(0f,0f,0f);
+        KnifeRB.useGravity =false;                                      
+        KnifeRB.isKinematic = true;                                                                 //拿起后摆正
+
 
         Vector3 delPos = (Input.mousePosition - lastMousePos) * 0.001f;
         //Vector3 pos = new Vector3();
