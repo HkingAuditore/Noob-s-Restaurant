0d1864494a92995d55c5746b396fb74f02ce5dec
WelkinTotoro <WelkinTotoro@outlook.com>
2018-12-05 23:29:52 +0800

    简单人物控制和动画

--- Assets/Script/PlayerMove.cs
+++ Assets/Script/PlayerMove.cs
@@ -2,45 +2,61 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMove : MonoBehaviour {
-    private Rigidbody player_rigbody;
+public class PlayerMove : MonoBehaviour
+{
+    private Rigidbody rig;
+    private PlayerInput input;
+    private Animator animator;
     public float rotate_speed;
     public float speed;
 
-    void Move(){
-        if (Input.GetKey(KeyCode.W))
+
+
+    void Move()
+    {
+        //if (Input.GetKey(KeyCode.W))
+        //{
+        //    player_rigbody.velocity = transform.forward * speed;
+        //    if (Input.GetKey(KeyCode.A))
+        //    {
+        //        this.transform.Rotate(0, -1 * rotate_speed * Time.deltaTime, 0);
+        //    }
+        //    if (Input.GetKey(KeyCode.D))
+        //    {
+        //        this.transform.Rotate(0, 1 * rotate_speed * Time.deltaTime, 0);
+        //    }
+        //}
+        //if (Input.GetKey(KeyCode.S))
+        //{
+        //    player_rigbody.velocity = -1*transform.forward * speed;
+        //    if (Input.GetKey(KeyCode.A))
+        //    {
+        //        this.transform.Rotate(0, 1 * rotate_speed * Time.deltaTime, 0);
+        //    }
+        //    if (Input.GetKey(KeyCode.D))
+        //    {
+        //        this.transform.Rotate(0, -1 * rotate_speed * Time.deltaTime, 0);
+        //    }
+        //}
+        if (Mathf.Abs(input.hor) > 1e-6 || Mathf.Abs(input.ver) > 1e-6)
         {
-            player_rigbody.velocity = transform.forward * speed;
-            if (Input.GetKey(KeyCode.A))
-            {
-                this.transform.Rotate(0, -1 * rotate_speed * Time.deltaTime, 0);
-            }
-            if (Input.GetKey(KeyCode.D))
-            {
-                this.transform.Rotate(0, 1 * rotate_speed * Time.deltaTime, 0);
-            }
+            transform.position += new Vector3(input.hor * speed * Time.deltaTime, 0, input.ver * speed * Time.deltaTime);
+            transform.LookAt(transform.position + Vector3.right * input.hor + Vector3.forward * input.ver);
+
+            animator.SetFloat("walk", Mathf.Sqrt(input.hor * input.hor + input.ver * input.ver));
         }
-        if (Input.GetKey(KeyCode.S))
-        {
-            player_rigbody.velocity = -1*transform.forward * speed;
-            if (Input.GetKey(KeyCode.A))
-            {
-                this.transform.Rotate(0, 1 * rotate_speed * Time.deltaTime, 0);
-            }
-            if (Input.GetKey(KeyCode.D))
-            {
-                this.transform.Rotate(0, -1 * rotate_speed * Time.deltaTime, 0);
-            }
-        }
-        
     }
 
-    void Start(){
-        player_rigbody = this.GetComponent<Rigidbody>();
+    void Start()
+    {
+        rig = this.GetComponent<Rigidbody>();
+        input = GetComponent<PlayerInput>();
+        animator = GetComponent<Animator>();
     }
 
 
-    void FixedUpdate () {
+    void FixedUpdate()
+    {
         Move();
     }
 }
