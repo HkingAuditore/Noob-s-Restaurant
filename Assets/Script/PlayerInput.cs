49c371aaaab47a1cc088535452e458daec677012
WelkinTotoro <WelkinTotoro@outlook.com>
2018-12-05 23:29:52 +0800

    简单人物控制和动画

---
+++ Assets/Script/PlayerInput.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInput : MonoBehaviour
+{
+
+    public float ver;
+    public float hor;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ver = Input.GetAxis("Vertical");
+        hor = Input.GetAxis("Horizontal");
+    }
+}
