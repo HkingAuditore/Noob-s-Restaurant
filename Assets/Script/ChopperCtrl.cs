98eac8addd94ce04c43229d9012a88cbc84d9bb1
WelkinTotoro <WelkinTotoro@outlook.com>
2018-12-10 23:11:00 +0800

    改变锅铲操控

--- Assets/Script/ChopperCtrl.cs
+++ Assets/Script/ChopperCtrl.cs
@@ -7,7 +7,7 @@ public class ChopperCtrl : MonoBehaviour
     public bool isCtrlling = false;
 
     private Vector3 oriPos;
-    private Rigidbody KnifeRB;
+    private Rigidbody rb;
     public float offsetY = 0.3f;
     public float offsetZ = 0.8f;
 
@@ -18,7 +18,7 @@ public class ChopperCtrl : MonoBehaviour
     {
         oriPos = transform.localPosition;
         lastMousePos = new Vector3(Screen.width / 2, Screen.height / 2, 0);
-        KnifeRB = GetComponent<Rigidbody>();
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
@@ -26,14 +26,14 @@ public class ChopperCtrl : MonoBehaviour
     {
         if (!isCtrlling)
         {
-            KnifeRB.useGravity = true;          //当玩家不使用时受重力控制
-            KnifeRB.isKinematic = false;
+            rb.useGravity = true;          //当玩家不使用时受重力控制
+            rb.isKinematic = false;
             return;
         }
 
         this.transform.rotation = Quaternion.Euler(0f, 0f, 0f);
-        KnifeRB.useGravity = false;
-        KnifeRB.isKinematic = true;                                                                 //拿起后摆正
+        rb.useGravity = false;
+        rb.isKinematic = true;                                                                 //拿起后摆正
 
 
         Vector3 delPos = (Input.mousePosition - lastMousePos) * 0.001f;
