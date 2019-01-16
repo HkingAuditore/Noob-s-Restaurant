6b4d81473d9be5485f737ad569ed74098c2f93a6
WelkinTotoro <WelkinTotoro@outlook.com>
2019-01-16 19:15:01 +0800

    将容器行为整合到Table基类中，在三种能用的桌子上增加放置容器的设定
    容器在桌子上摆放的逻辑直接沿用了原选材桌的逻辑，但由于桌子方向不同使得所用本地坐标的方向在每个桌子上都不同
    摆放的顺序须按选材桌的(世界坐标)从x大到小，从z小到大的顺序，或者使用Table脚本上增加的ResetWaresPose方法(Editor中使用)

--- Assets/Script/Table/BTable.cs
+++ Assets/Script/Table/BTable.cs
@@ -7,8 +7,53 @@ public sealed class BTable : Table {
     [SerializeField]
     GameObject bCamera;
 
+    protected override void Awake()
+    {
+        base.Awake();
+
+        thisRowMaxPlaceNum = 2;
+        thisMaxPlaceNum = 4;
+        thisColumnFoodSetSpace = 5.85f;
+        thisRowFoodSetSpace = -2.02f;
+
+        wareSet = new List<Container>(thisMaxPlaceNum);
+    }
+
     protected override void GetCamera()
     {
         cameraGO = bCamera;
+    }
+
+    protected override void SelectFoodSet()
+    {
+        base.SelectFoodSet();
+        if (currentChosenWare != null)
+        {
+            if (Input.GetMouseButtonDown(1) && selectFoodSetCoroutine == null)
+            {
+                PutFoodSetBack();
+            }
+        }
+    }
+
+    protected override void SelectMethod()
+    {
+        base.SelectMethod();
+        PutFoodSetBack();
+        wareSet[wareSetIndex].transform.position = preelectionFoodSetTrans.position;
+        currentChosenWare = wareSet[wareSetIndex].gameObject.GetComponent<Ware>();
+        wareSet[wareSetIndex] = null;
+    }
+
+    protected override void OnEnterTable()
+    {
+        base.OnEnterTable();
+        PutWareOnTablePreelectionPos();
+    }
+
+    protected override void OnQuitTable()
+    {
+        base.OnQuitTable();
+        GivePlayerSelectedWare();
     }
 }
--- Assets/Script/Table/CTable.cs
+++ Assets/Script/Table/CTable.cs
@@ -2,13 +2,61 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public sealed class CTable : Table {
+public sealed class CTable : Table, IContainable<Container>
+{
 
     [SerializeField]
     GameObject cCamera;
+
+    protected override void Awake()
+    {
+        base.Awake();
+
+        thisRowMaxPlaceNum = 2;
+        thisMaxPlaceNum = 4;
+        thisColumnFoodSetSpace = 4.41f;
+        thisRowFoodSetSpace = -1.89f;
+
+        wareSet = new List<Container>(thisMaxPlaceNum);
+    }
 
     protected override void GetCamera()
     {
         cameraGO = cCamera;
     }
+
+    protected override void SelectFoodSet()
+    {
+        base.SelectFoodSet();
+        if (currentChosenWare != null)
+        {
+            if (Input.GetMouseButtonDown(1) && selectFoodSetCoroutine == null)
+            {
+                PutFoodSetBack();
+            }
+        }
+    }
+
+    protected override void SelectMethod()
+    {
+        base.SelectMethod();
+        PutFoodSetBack();
+        wareSet[wareSetIndex].transform.position = preelectionFoodSetTrans.position;
+        currentChosenWare = wareSet[wareSetIndex].gameObject.GetComponent<Ware>();
+        wareSet[wareSetIndex] = null;
+    }
+
+    protected override void OnEnterTable()
+    {
+        base.OnEnterTable();
+        PutWareOnTablePreelectionPos();
+    }
+
+    protected override void OnQuitTable()
+    {
+        base.OnQuitTable();
+        GivePlayerSelectedWare();
+    }
+
+
 }
