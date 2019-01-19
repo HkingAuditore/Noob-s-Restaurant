04908b552576591f089f615690f752a3b4b5f477
Sukhoi-air <zjq950313@gmail.com>
2019-01-19 18:07:47 +0800

    ui

---
+++ Assets/Script/UI/StartScene.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartScene : MonoBehaviour
+{
+    public void OnClickStartGame(string strChangeSceneName)
+    {
+        SceneManager.LoadScene(strChangeSceneName);
+    }
+
+    public void OnClickQuitGame()
+    {
+#if UNITY_EDITOR
+        EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+    }
+}
