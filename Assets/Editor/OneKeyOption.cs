623e97b321faafb6ff76bd4e8f98b8f11183c00d
WelkinTotoro <WelkinTotoro@outlook.com>
2019-01-21 21:06:38 +0800

    在编辑器中增加一键更改画质的选项让我垃圾的电脑不会那么卡

---
+++ Assets/Editor/OneKeyOption.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.PostProcessing;
+using UnityStandardAssets.CinematicEffects;
+
+public class OneKeyOption : Editor
+{
+    [MenuItem("One Key Option/Performance/Separate/ReflectionProbe/Enable")]
+    private static void EnableAllReflectionProbe()
+    {
+        ReflectionProbe[] rps = FindObjectsOfType<ReflectionProbe>();
+        Array.ForEach(rps, (rp) => { rp.enabled = true; rp.mode = UnityEngine.Rendering.ReflectionProbeMode.Baked; });
+
+        Debug.Log(rps.Length + " Reflection Probes were enabled");
+    }
+
+    [MenuItem("One Key Option/Performance/Separate/ReflectionProbe/Disable")]
+    private static void DisableAllReflectionProbe()
+    {
+        ReflectionProbe[] rps = FindObjectsOfType<ReflectionProbe>();
+        Array.ForEach(rps, (rp) => rp.enabled = false);
+
+        Debug.Log(rps.Length + " Reflection Probes were disabled");
+    }
+
+    [MenuItem("One Key Option/Performance/Separate/Light/Enable")]
+    private static void EnableAllLight()
+    {
+        Light[] lights = FindObjectsOfType<Light>();
+        Array.ForEach(lights, (rp) => { rp.enabled = true; });
+
+        Debug.Log(lights.Length + " Lights were enabled");
+    }
+
+    [MenuItem("One Key Option/Performance/Separate/Light/Disable")]
+    private static void DisableAllLight()
+    {
+        Light[] lights = FindObjectsOfType<Light>();
+        Array.ForEach(lights, (rp) => rp.enabled = false);
+
+        Debug.Log(lights.Length + " Lights were disabled");
+    }
+
+    [MenuItem("One Key Option/Performance/Separate/PostProcessing/Enable")]
+    private static void EnablePostProcessing()
+    {
+        GameObject camera = GameObject.Find("Camera/Main Camera");
+        PostProcessingBehaviour pp = camera.GetComponent<PostProcessingBehaviour>();
+        AntiAliasing aa = camera.GetComponent<AntiAliasing>();
+        MotionBlur mb = camera.GetComponent<MotionBlur>();
+        pp.enabled = true;
+        aa.enabled = true;
+        mb.enabled = true;
+
+        Debug.Log("Post Processings were enabled");
+    }
+
+    [MenuItem("One Key Option/Performance/Separate/PostProcessing/Disable")]
+    private static void DisablePostProcessing()
+    {
+        GameObject camera = GameObject.Find("Camera/Main Camera");
+        PostProcessingBehaviour pp = camera.GetComponent<PostProcessingBehaviour>();
+        AntiAliasing aa = camera.GetComponent<AntiAliasing>();
+        MotionBlur mb = camera.GetComponent<MotionBlur>();
+        pp.enabled = false;
+        aa.enabled = false;
+        mb.enabled = false;
+
+        Debug.Log("Post Processings were disabled");
+    }
+
+    [MenuItem("One Key Option/Performance/OverallQuality/High")]
+    private static void EnableHighQuality()
+    {
+        EnableAllReflectionProbe();
+        EnableAllLight();
+        EnablePostProcessing();
+    }
+
+    [MenuItem("One Key Option/Performance/OverallQuality/Low")]
+    private static void EnableLowQuality()
+    {
+        DisableAllReflectionProbe();
+        DisableAllLight();
+        DisablePostProcessing();
+    }
+}
