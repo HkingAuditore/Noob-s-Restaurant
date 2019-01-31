b01ceb7049716f00b093c4465971bd9cd82111a4
HkingAuditore <178854663@qq.com>
2019-01-31 20:58:44 +0800

    加热时间足够时改材质

--- Assets/Script/Entity/Food/Tomato.cs
+++ Assets/Script/Entity/Food/Tomato.cs
@@ -4,9 +4,34 @@ using UnityEngine;
 
 public class Tomato : Ingredient
 {
+
+    public Material leafHeatedMat;
+    public Material ballHeatedMat;
+
+
+
     protected override void Start()
     {
         base.Start();
         FoodName = FoodName.Tomato;
     }
+
+
+
+    private void Update()
+    {
+        ChangeMat();
+       // Debug.Log(HeatTime);
+    }
+
+
+    void ChangeMat()
+    {
+        if (HeatTime > targetTime)
+        {
+            transform.Find("叶").gameObject.GetComponent<MeshRenderer>().sharedMaterial = leafHeatedMat;
+            transform.Find("果").gameObject.GetComponent<MeshRenderer>().sharedMaterial = ballHeatedMat;
+        }
+    }
+
 }
