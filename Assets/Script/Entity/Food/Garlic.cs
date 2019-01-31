f37c5e5f6c57d6ae0b6b65a94ec4cf7ae71a504f
HkingAuditore <178854663@qq.com>
2019-01-31 21:34:49 +0800

    加入葱和蒜

---
+++ Assets/Script/Entity/Food/Garlic.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Garlic : Ingredient
+{
+
+    //public Material leafHeatedMat;
+   // public Material ballHeatedMat;
+
+
+
+    protected override void Start()
+    {
+        base.Start();
+        FoodName = FoodName.Garlic;
+    }
+
+
+
+   /* private void Update()
+    {
+        //ChangeMat();
+        // Debug.Log(HeatTime);
+    }*/
+
+
+   /* void ChangeMat()
+    {
+        if (HeatTime > targetTime)
+        {
+            transform.Find("叶").gameObject.GetComponent<MeshRenderer>().sharedMaterial = leafHeatedMat;
+            transform.Find("果").gameObject.GetComponent<MeshRenderer>().sharedMaterial = ballHeatedMat;
+        }
+    }*/
+
+}
