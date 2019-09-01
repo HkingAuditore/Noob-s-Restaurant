3c086d87bd16abea0b900826f69f08b82dbc6b4f
HkingAuditore <178854663@qq.com>
2019-09-01 23:58:32 +0800

    没写完的原材料遮罩

---
+++ Assets/Editor/DishesRendererGUI.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(DishesRenderer))]
+public class DishesRendererGUI : Editor {
+	private string[] _SelStrings = {"None", "Fry", "Stir", "Boil"};
+
+
+	public int _Selected=0;
+
+	
+    public override void OnInspectorGUI(){
+        DrawDefaultInspector();
+        DishesRenderer dishesRenderGUI = (DishesRenderer) target;
+        _Selected=GUILayout.SelectionGrid(_Selected,_SelStrings,4);
+    	if(GUILayout.Button("Add Process")){
+            dishesRenderGUI.AddProcess((DishesRenderer.ProcessType)_Selected);
+        }
+        if (GUILayout.Button("Remove Process"))
+        {
+            dishesRenderGUI.RemoveProcess();
+        }
+
+    }
+}
---
+++ Assets/Script/ProcessOriginal/DishesRenderer.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+[ExecuteInEditMode]
+public class DishesRenderer : MonoBehaviour {
+
+	public enum ProcessType {none=0,fry,stir,boil}
+	private List<ProcessType> _ProcessTags = new List<ProcessType>();
+    public Material[] _ProcessMaterials = new Material[4];
+
+
+    private Material[] _UsedMaterials = new Material[10];
+    //private int _NumofProcess = 0;
+
+	public void AddProcess(ProcessType _processtype){
+		this._ProcessTags.Add(_processtype);
+        _UsedMaterials[(int)_ProcessTags.Count-1] = _ProcessMaterials[(int)_processtype];
+        this.GetComponent<Renderer>().materials = _UsedMaterials;
+	}
+
+    public void RemoveProcess()
+    {
+        this._ProcessTags.RemoveAt(_ProcessTags.Count-1);
+        _UsedMaterials[(int)_ProcessTags.Count] = null;
+        this.GetComponent<Renderer>().materials = _UsedMaterials;
+    }
+}
